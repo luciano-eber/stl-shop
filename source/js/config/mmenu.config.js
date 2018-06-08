0931095dd4641fa020877e12fb2786d070bf2b94
luciano-lesti <lucianochitao1995@hotmail.com>
2018-06-08 13:56:17 -0300

    Planejamento e teste do esqueleto seguindo o mmenu plugin ok

--- source/js/config/mmenu.config.js
+++ source/js/config/mmenu.config.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
-    $("#my-menu").mmenu({
-       // options
-    }, {
-       // configuration
-       offCanvas: {
-          pageSelector: "#page"
-       }
-    });
 
+    $("#my-menu").mmenu({
+        // options
+     }, {
+        // configuration
+        offCanvas: {
+           pageSelector: "#my-page"
+        }
+     });
+
+    var API = $("#my-menu").data( "mmenu" );
+
+    $("#my-button").click(function() {
+        $(triggerToggle( $(this), API.open, API.close ) );
+     });
+    
 });
+
+function triggerToggle(trigger,open,close){
+
+    let status = trigger.attr('status');
+
+    if( status == 'close' ){
+        open();
+        trigger.attr('status','open');
+    } else {
+        close();
+        trigger.attr('status','close');
+    }
+}
