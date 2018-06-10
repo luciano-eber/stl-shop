57140bb9d052c3244fba994665f4618019b2f18e
luciano-lesti <lucianochitao1995@hotmail.com>
2018-06-10 15:05:37 -0300

    progresso até o momento, iniciando solução rwd para painel courtney

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,6 @@
 
 //slider-main
-var swiper = new Swiper('.slider-main', {
+var sliderMain = new Swiper('.slider-main', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -10,4 +10,28 @@ var swiper = new Swiper('.slider-main', {
       disableOnInteraction: true,
     },
     loop: true,
+});
+
+//marcas em destaque
+var brands = new Swiper('.brands', {
+    slidesPerView: 6,
+	spaceBetween: 10,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+    loop: true,
+});
+
+//shop-the-look
+var shopTheLook = new Swiper('.shop-the-look', {
+	slidesPerView: 3,
+	spaceBetween: 15,
+	breakpoints: {
+		992: {
+		  slidesPerView: 2,
+		  spaceBetween: 30,
+		},
+	},
+	loop: true,
 });
